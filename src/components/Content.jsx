b7ac62cdb42943095589c960609f88ebfb52f646
ELIGETI RAVI TEJA <eligetiraviteja267@gmail.com>
2024-08-13 14:06:48 +0530

    midified

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Navbar } from './Navbar'
 import axios from "axios"
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,28 @@ import React from 'react'
 export const Navbar = ({display}) => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar bg-primary" data-bs-theme="dark">
-  <div class="container-fluid">
-    <h2 class="navbar-brand">Navbar</h2>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar bg-primary" data-bs-theme="dark">
+  <div className="container-fluid">
+    <h2 className="navbar-brand">Navbar</h2>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <button class="nav-link active" aria-current="page" onClick={display}>Tasks</button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <button className="nav-link active" aria-current="page" onClick={display}>Tasks</button>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
         </li>
-        <li class="nav-item dropdown">
+        <li className="nav-item dropdown">
          
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
      </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
